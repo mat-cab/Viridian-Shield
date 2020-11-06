b27dfb51567b99b951076d261735cf6ffda5deb5
Mathieu Cabasson <mat-cab@users.noreply.github.com>
2020-11-06 11:31:32 +0100

    debug: correction of serial start

--- src/debug/debug.cpp
+++ src/debug/debug.cpp
@@ -5,31 +5,35 @@
 void debug::initialize() {
     // debug generally only available for MEGA on Serial1
     #ifdef ARDUINO_AVR_MEGA2560
-    if (!Serial1) {
-        Serial1.begin(DEBUG_SERIAL_SPEED);
-    }
+    // Open Serial
+    Serial.begin(DEBUG_SERIAL_SPEED);
+    // Wait for serial to be ready
+    while (!Serial) ;
     #endif
 
     // possibility to force debug output to Serial on UNO
     // Note that it will conflict with Teleinfo in (on RX)
     // Therefore switch RX_IN must be OFF to properly use this debug on Uno
     #ifdef ARDUINO_AVR_UNO
-    if (DEBUG_FORCE && !Serial) {
+    if (DEBUG_FORCE) {
+        // Open Serial
         Serial.begin(DEBUG_SERIAL_SPEED);
+        // Wait for serial to be ready
+        while (!Serial) ;
     }
     #endif
 }
 
-void debug::log(const char* message) {
+void debug::log(const String message) {
     // debug generally only available for MEGA on Serial1
     #ifdef ARDUINO_AVR_MEGA2560
-        Serial1.write(message);
+    Serial.println(message);
     #endif
 
     // debug on Uno if forced
     #ifdef ARDUINO_AVR_UNO
     if (DEBUG_FORCE) {
-        Serial.write(message);
+        Serial.println(message);
     }
     #endif
 }
--- src/debug/debug.h
+++ src/debug/debug.h
@@ -4,7 +4,7 @@
 
 #ifdef ARDUINO_AVR_UNO
 // Option to force the debug on Serial for the Arduino UNO
-static const boolean DEBUG_FORCE = false;
+static const boolean DEBUG_FORCE = true;
 #endif
 
 // Standard Serial Speed
@@ -13,5 +13,5 @@ static const int DEBUG_SERIAL_SPEED = 9600;
 class debug {
     public:
         static void initialize();
-        static void log(const char* message);
+        static void log(const String message);
 };
