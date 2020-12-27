3dee9a2d7a9f6876fefd6bfc36307c8b8104ea49
Mathieu Cabasson <mat-cab@users.noreply.github.com>
2020-12-27 17:15:15 +0100

    debug: removed FORCE_DEBUG for UNO
    This option is probably not working, because Serial is used for Teleinfo

--- src/debug/debug.h
+++ src/debug/debug.h
@@ -2,11 +2,6 @@
 
 #include <Arduino.h>
 
-#ifdef ARDUINO_AVR_UNO
-// Option to force the debug on Serial for the Arduino UNO
-static const boolean DEBUG_FORCE = true;
-#endif
-
 // Standard Serial Speed
 static const int DEBUG_SERIAL_SPEED = 9600;
 
