97800aa132e49ddcc004b64106ce8772fa10c957
Mathieu Cabasson <mat-cab@users.noreply.github.com>
2021-05-29 13:28:29 +0200

    debug: Always use Serial for debugging
    On Uno and on Mega

--- src/debug/debug.cpp
+++ src/debug/debug.cpp
@@ -3,37 +3,16 @@
 #include "debug.h"
 
 void debug::initialize() {
-    // debug generally only available for MEGA on Serial1
-    #ifdef ARDUINO_AVR_MEGA2560
-    // Set pins for FTD 1232 on pins 14 to 18:
-    // DTR is not on an Arduino pin
-    // pin 14 is RX (on FTD)
-    // pin 15 is TX (on FTD)
-    // pin 16 is VCC
-    pinMode(16, OUTPUT);
-    digitalWrite(16, HIGH);
-    // pin 17 is CTS
-    pinMode(17, INPUT);
-    // pin 18 is GND
-    pinMode(18, OUTPUT);
-    pinMode(18, LOW);
     // Open Serial
-    Serial3.begin(DEBUG_SERIAL_SPEED);
+    Serial.begin(DEBUG_SERIAL_SPEED);
     // Wait for serial to be ready
-    while (!Serial3) ;
-    #endif
+    while (!Serial) ;
 }
 
 void debug::log(const String message) {
-    // debug generally only available for MEGA on Serial1
-    #ifdef ARDUINO_AVR_MEGA2560
-    Serial3.println(message);
-    #endif
+    Serial.println(message);
 }
 
 void debug::logNoLine(const String message) {
-    // debug generally only available for MEGA on Serial1
-    #ifdef ARDUINO_AVR_MEGA2560
-    Serial3.print(message);
-    #endif
+    Serial.print(message);
 }
