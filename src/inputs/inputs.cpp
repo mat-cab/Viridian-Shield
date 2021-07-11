53c98cd1a3b9824b9d94a8eae55c4a2f1c9f945d
Mathieu Cabasson <mat-cab@users.noreply.github.com>
2021-07-11 13:05:26 +0200

    inputs: Corrected initialization for uno and V02 shield

--- src/inputs/inputs.cpp
+++ src/inputs/inputs.cpp
@@ -3,14 +3,18 @@
 #include "inputs.h"
 
 void inputs::initialize() {
-    // set pins 2 to 9 in INPUT PULLUP mode
-    for (uint8_t i = 2; i < 10; i++) {
+    // set pins 2 to 8 in INPUT PULLUP mode
+    for (uint8_t i = 2; i < 8; i++) {
         pinMode(i, INPUT_PULLUP);
     }
 
+    // set digital/analog pins to INPUT
+    pinMode(16, INPUT_PULLUP);
+    pinMode(17, INPUT_PULLUP);
+
     // set analog pins to INPUT
-    pinMode(23, INPUT);
-    pinMode(24, INPUT);
+    pinMode(14, INPUT);
+    pinMode(15, INPUT);
 }
 
 bool inputs::readOption(uint8_t pin) {
