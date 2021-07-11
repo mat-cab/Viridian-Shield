767c895d8c020948cee86d5b674f686ff04e4a52
Mathieu Cabasson <mat-cab@users.noreply.github.com>
2021-07-11 12:53:33 +0200

    inputs: Updated adressing for V02 shield

--- src/inputs/inputs.h
+++ src/inputs/inputs.h
@@ -4,11 +4,11 @@
 
 // Association between options and pins
 // pin 8 is to use the multiplier in front of ISOUSC
-static const uint8_t INPUTS_OPTION_GREATER_ISOUSC = 8;
+static const uint8_t INPUTS_OPTION_GREATER_ISOUSC = 7;
 // pin 9 is to add 1A to the current margin
-static const uint8_t INPUTS_OPTION_MARGIN_ADD_1A = 9;
+static const uint8_t INPUTS_OPTION_MARGIN_ADD_1A = 8;
 // pin 7 is the signal when the car is charging
-static const uint8_t INPUTS_OPTION_CHARGING_CAR = 7;
+static const uint8_t INPUTS_OPTION_CHARGING_CAR = 6;
 
 // Association between variables and pins
 
